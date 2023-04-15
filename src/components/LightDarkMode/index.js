785e5071b7e6157a538a0a48a0e57146bfba6e22
boyajayanthi100ABC <boyajayanthi100@gmail.com>
2023-04-15 13:30:51 +0000

    clicktochange solution

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -1,32 +1,70 @@
-// Write your code here
+/* Write your code here
 
 import './index.css'
 import {Component} from 'react'
 
 class LightDarkMode extends Component {
-    state ={mode: true}
-    lightMode = () => {
-        this.setState(() = {
-            console.log("lightMode")
-            return{mode: false}
-        })
-    }
+  state = {isDarkMode: true}
 
-    darkMode = () => {
-        this.setState(() => {
-            console.log("darkMode")
-            return{mode: true}
-        })
-    }
+  onClickButton = () => {
+    this.setState(prevState => ({prevState: isDarkMode}))
+  }
 
   render() {
-      {mode} = this.state
+    const {isDarkMode} = this.state
+    const modeClassName = isDarkMode ? 'dark-mode' : 'light-mode'
+    const buttonText = isDarkMode ? 'Light Mode' : 'Dark Mode'
+
     return (
-      <div className="bg-container">
-        <h1 className="heading"> click To change Mode </h1>
-        <button className="button"> Light Mode </button>
-        {mode? (<button className="button" onClick = {this.lightMode}> Light Mode </button>): (<button className="button"  onClick = {this.darkMode}> Dark Mode </button>)}
+      <div className="app-container">
+        <div className={`container ${modeClassName}`}>
+          <h1 className="heading"> Click To Change Mode </h1>
+          <button type="button" onClick={this.onClickButton} className="button">
+            {buttonText}
+          </button>
+        </div>
       </div>
     )
   }
 }
+
+export default LightDarkMode*/
+
+import './index.css'
+import {Component} from 'react'
+
+class LightDarkMode extends Component {
+  state = {mode: true, text: 'Light Mode'}
+
+  lightMode = () => {
+    this.setState({mode: false, text: 'Dark Mode'})
+  }
+
+  darkMode = () => {
+    this.setState({mode: true, text: 'Light Mode'})
+  }
+  render() {
+    const {mode, text} = this.state
+
+    return(
+       { mode ?(
+      <div className="bg-dark-container">
+        <h1 className="heading"> click To change Mode </h1>
+        <button className="button" onClick={this.lightMode}>
+          {text}
+        </button>
+      </div>
+    ) : (
+      <div className="bg-light-container">
+        <h1 className="heading"> click To change Mode </h1>
+        <button className="button" onClick={this.darkMode}>
+          {' '}
+          {text}{' '}
+        </button>
+      </div>
+    )
+  }})
+}
+}
+
+export default LightDarkMode
